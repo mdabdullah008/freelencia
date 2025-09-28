9244c98612af3118b125b26cec35673cc5df0501
Night-Rider64 <areezahasanrayat@gmail.com>
2025-09-28 21:52:27 +0600

    corrected issues in freelancer/models and shastafiverr/urls

--- shastafiverr/freelancer/models.py
+++ shastafiverr/freelancer/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here please.
 
-class UserProfieInfo(models.Model):
+class UserProfileInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
