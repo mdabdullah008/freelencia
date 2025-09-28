dc156e9a3a887f89efe2165ddf21b3ce44ee8178
mdabdullah008 <mdabdullahashfaq008@gmail.com>
2025-09-28 22:55:43 +0600

    yes i forgot to add slashes in the end, im so stupid

--- shastafiverr/freelancer/urls.py
+++ shastafiverr/freelancer/urls.py
@@ -3,9 +3,9 @@ from django.urls import re_path
 app_name = 'freelancer'
 
 urlpatterns = [
-	re_path(r'register', views.register, name='register'),
+	re_path(r'register/', views.register, name='register'),
 
-	re_path(r'user_login', views.user_login, name='user_login'),
+	re_path(r'user_login/', views.user_login, name='user_login'),
 ]
 
 
