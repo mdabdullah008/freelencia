0f3c926c4d6e39d05f708d72eb99218ff936117f
Night-Rider64 <areezahasanrayat@gmail.com>
2025-09-28 22:28:41 +0600

    fixed issue in freelancer/views where abdu put a parenthesis where it shouldnt be

--- shastafiverr/freelancer/views.py
+++ shastafiverr/freelancer/views.py
@@ -50,7 +50,7 @@ def user_login(request):
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
         if user:
-            if user.is_active():
+            if user.is_active:
                 login(request, user)
                 return HttpResponseRedirect(reverse('index'))
             else:
