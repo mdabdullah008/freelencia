99ad9295ffe877d0614c169e9bc962b2ded3f723
Mohammed Abdullah (Ashfaq) <ipsc42mohammedabdullah@gmail.com>
2025-10-03 00:25:55 +0600

    test

--- shastafiverr/freelancer/models.py
+++ shastafiverr/freelancer/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here please.
+# Create your models here.
 
 class UserProfileInfo(models.Model):
     ROLE_CHOICES = [
