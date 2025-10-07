3d95151a976f2d2d57863849c5eca5dd878c51ba
mdabdullah008 <mdabdullahashfaq008@gmail.com>
2025-10-08 01:26:51 +0600

    removed unecessary imports

--- shastafiverr/hub/forms.py
+++ shastafiverr/hub/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Category, UserProfileInfo
-from .models import ProgrammingTech, GraphicsDesign, VideoAnimation, Business
 from django import forms
 from .models import ClientRequest
 
