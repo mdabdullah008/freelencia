d7e25564f5f22bcff0e89357b4a15078dc2f4e6b
mdabdullah008 <mdabdullahashfaq008@gmail.com>
2025-10-09 00:03:10 +0600

    deleted unecessary models

--- shastafiverr/hub/models.py
+++ shastafiverr/hub/models.py
@@ -54,50 +54,6 @@ class Job(models.Model):
 
     def __str__(self):
         return self.title
-    
-#Programming & Tech Table
-class ProgrammingTech(models.Model):
-    name = models.CharField(max_length=100)
-    profile_pic = models.ImageField(upload_to='profiles/')
-    email = models.EmailField(max_length=100)
-    age = models.IntegerField()
-    education = models.CharField(max_length=200)
-
-def __str__(self):
-    return self.name
-
-#Graphics & Design Table
-class GraphicsDesign(models.Model):
-    name = models.CharField(max_length=100)
-    profile_pic = models.ImageField(upload_to='profiles/')
-    email = models.EmailField(max_length=100)
-    age = models.IntegerField()
-    education = models.CharField(max_length=200)
-
-def __str__(self):
-    return self.name
-
-#Video & Animation Table
-class VideoAnimation(models.Model):
-    name = models.CharField(max_length=100)
-    profile_pic = models.ImageField(upload_to='profiles/')
-    email = models.EmailField(max_length=100)
-    age = models.IntegerField()
-    education = models.CharField(max_length=200)
-
-def __str__(self):
-    return self.name
-
-#Business Table
-class Business(models.Model):
-    name = models.CharField(max_length=100)
-    profile_pic = models.ImageField(upload_to='profiles/')
-    email = models.EmailField(max_length=100)
-    age = models.IntegerField()
-    education = models.CharField(max_length=200)
-
-def __str__(self):
-    return self.name
 
 class ClientRequest(models.Model):
     STATUS_CHOICES = [
