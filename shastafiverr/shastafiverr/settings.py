169d7c5fad4dfbf5ed06e50329656f429d743deb
mdabdullah008 <mdabdullahashfaq008@gmail.com>
2025-10-08 23:43:29 +0600

    fixed email connection issues

--- shastafiverr/shastafiverr/settings.py
+++ shastafiverr/shastafiverr/settings.py
@@ -129,3 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'freelancingautomation@gmail.com'
+EMAIL_HOST_PASSWORD = 'hmsyfexgquxxxzwt'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
